b51ccc4c76b2e9dfa7878502fedc1c200bb63615
NourigeonL <louis.nourigeon@gmail.com>
2022-03-09 13:26:42 +0900

    created tests for existing functions

---
+++ tests/test_myfunctions.py
@@ -0,0 +1,12 @@
+from magicLib import magicfunctions
+
+def test_magicFunction1():
+    assert magicfunctions.magicFunction1("vahbunA") == "Anubhav"
+
+def test_magicFunction2():
+    arr = [0,0,0,0,1,0,0,0,0]
+    assert magicfunctions.magicFunction2(arr, 1) == 4
+
+def test_magicFunction3():
+    arr = ["Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz"]
+    assert magicfunctions.magicFunction3(9,15) == arr
