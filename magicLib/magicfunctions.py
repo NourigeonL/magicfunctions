d58fc38bcfbb079430ef0e845cc12d85364f7f68
NourigeonL <louis.nourigeon@gmail.com>
2022-03-09 13:26:02 +0900

    updated file names

--- magicLib/magicfunctions.py
+++ magicLib/magicfunctions.py
@@ -54,7 +54,7 @@ def magicFunction3(start: int, end: int) -> list[str]:
     """
 
     result = []
-    for i in range(start,end):
+    for i in range(start,end+1):
         if i % 3 == 0 and i % 5 == 0:
             result.append("FizzBuzz")
         elif i % 3 == 0:
